1d42f9645cf264d8d652c728ad0cc14761bf8e83
Ricardo Pinho <ricardo.pinho@gisvm.com>
2018-08-01 15:07:34 +0100

    Update arMoaThemePluginConfiguration.class.php
    indenpendent plugin name and location

--- config/arMoaThemePluginConfiguration.class.php
+++ config/arMoaThemePluginConfiguration.class.php
@@ -5,23 +5,26 @@ class arMoaThemePluginConfiguration extends sfPluginConfiguration
   // Summary and version. AtoM recognizes any plugin as a theme as long as
   // the $summary string contains the word "theme" in it (case-insensitive).
   public static
-    $summary = 'MOA Theme plugin, extension of arDominionPlugin. Cross-browser compatibility tested. Based in Twitter Bootstrap 2.0, 940px two-column layout, slightly responsive.',
+    $summary = 'Theme plugin, extension of arDominionPlugin. Cross-browser compatibility tested. Based in Twitter Bootstrap 2.0, 940px two-column layout, slightly responsive.',
     $version = '0.0.1';
 
   public function contextLoadFactories(sfEvent $event)
   {
     $context = $event->getSubject();
+    // Define pluginpath for indenpendent theme name location
+    $pluginpath = substr(__DIR__, strpos(__DIR__, "/plugins"));
+    $pluginpath = rtrim($pluginpath, "config");
 
     // Runtime less interpreter will be loaded if debug mode is enabled
     // Remember to avoid localStorage caching when dev machine is not localhost
     if ($context->getConfiguration()->isDebug())
     {
       $context->response->addJavaScript('/vendor/less.js');
-      $context->response->addStylesheet('/plugins/arMoaThemePlugin/css/main.less', 'last', array('rel' => 'stylesheet/less', 'type' => 'text/css', 'media' => 'all'));
+      $context->response->addStylesheet($pluginpath.'css/main.less', 'last', array('rel' => 'stylesheet/less', 'type' => 'text/css', 'media' => 'all'));
     }
     else
     {
-      $context->response->addStylesheet('/plugins/arMoaThemePlugin/css/main.css', 'last', array('media' => 'all'));
+      $context->response->addStylesheet($pluginpath.'css/main.css', 'last', array('media' => 'all'));
     }
   }
 
